5d437546d20ade653cb849a650bcdb44a65fa857
LuisF973 <luisfelipe.k.m.rodrigues@gmail.com>
2025-03-12 16:52:34 -0300

    atualizacao

--- revisao/src/components/Band/index.jsx
+++ revisao/src/components/Band/index.jsx
@@ -1,45 +1,16 @@
 import React from "react";
 import styles from "./Band.module.css";
 
-function Band() {
+function Band({imagem, nome, descricao}) {
 
     return (
-        <section className={styles.band}>
-            <div class="band">
-                <img src="https://cdn.ontourmedia.io/gunsnroses/site_v2/animations/gnr_loop_logo_01.jpg" alt="Banda 1"/>
-                <div class="band-info">
-                    <h2>Banda 1</h2>
-                    <p>Descrição da Banda 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
+        <div className={styles.band}>
+            <img src={imagem} alt={nome} />
+            <div className={styles.bandinfo}>
+                <h2>{nome}</h2>
+                <p>{descricao}</p>
             </div>
-        
-
-        
-            <div class="band">
-                <img src="https://m.media-amazon.com/images/I/91WaJU5cCXL._AC_UF1000,1000_QL80_.jpg" alt="Banda 2"/>
-                <div class="band-info">
-                    <h2>Banda 2</h2>
-                    <p>Descrição da Banda 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-            </div>
-        
-            <div class="band">
-                <img src="https://m.media-amazon.com/images/I/91WaJU5cCXL._AC_UF1000,1000_QL80_.jpg" alt="Banda 2"/>
-                <div class="band-info">
-                    <h2>Banda 3</h2>
-                    <p>Descrição da Banda 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-            </div>
-        
-            <div class="band">
-                <img src="https://m.media-amazon.com/images/I/91WaJU5cCXL._AC_UF1000,1000_QL80_.jpg" alt="Banda 2"/>
-                <div class="band-info">
-                    <h2>Banda 4</h2>
-                    <p>Descrição da Banda 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-            </div>
-        </section>
-
+        </div>
 
     );
 }
--- revisao/src/components/Main/index.jsx
+++ revisao/src/components/Main/index.jsx
@@ -1,10 +1,50 @@
 import React from "react";
-import Band from "../Band";
+import Band from "../Band/index";
+import styles from "./Main.module.css"
+
+
+const bandas = [
+    {
+        nome: 'Banda 1',
+        imagem: 'https://cdn.ontourmedia.io/gunsnroses/site_v2/animations/gnr_loop_logo_01.jpg',
+        descricao: 'Descrição da Banda 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+    {
+        nome: 'Banda 2',
+        imagem: 'https://m.media-amazon.com/images/I/91WaJU5cCXL._AC_UF1000,1000_QL80_.jpg',
+        descricao: 'Descrição da Banda 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+    {
+        nome: 'Banda 3',
+        imagem: 'https://m.media-amazon.com/images/I/91WaJU5cCXL._AC_UF1000,1000_QL80_.jpg',
+        descricao: 'Descrição da Banda 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+    {
+        nome: 'Banda 4',
+        imagem: 'https://m.media-amazon.com/images/I/91WaJU5cCXL._AC_UF1000,1000_QL80_.jpg',
+        descricao: 'Descrição da Banda 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+
+
+];
+
 
 function Main() {
     return (
-        <main>
-            <Band />
+        <main className={styles.main}>
+            {
+                bandas.map((banda, index) => (
+                    <section>
+                        <Band key={index} {...banda} />
+                    </section>
+
+
+                ))
+            }
+
+            {/* <section>
+            <Band imagem="https://static.vecteezy.com/ti/vetor-gratis/p3/2972281-simbolo-do-rock-n-roll-conceito-arte-do-rock-music-em-estilo-monocromatico-vetor.jpg" nome="Banda 1" descricao="Banda legal"/>
+            </section> */}
         </main>
     );
 }
